f6972f9aa08906fd8d41a6be33b701226d401257
Heyjune3 <kfd45674@gmail.com>
2024-01-02 13:49:52 +0900

    health Pro 홈페이지 메뉴 구현 중

---
+++ Leejuneho/src/application/TestMain.java
@@ -0,0 +1,27 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class TestMain extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/fxml/HomePage.fxml"));
+			Parent root = loader.load();
+			primaryStage.setScene(new Scene(root));
+			primaryStage.setTitle("Login 페이지");
+			primaryStage.setResizable(false);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ Leejuneho/src/application/dao/UserDAO.java
@@ -0,0 +1,15 @@
+package application.dao;
+
+import application.dto.User;
+
+public interface UserDAO {
+
+	// 회원가입
+	User join(User user);
+
+	// 아이디가 기존에 존재하는지 확인
+	boolean selectMember(String userId);
+
+	// Id와 Passwd가 일치하는 사용자 검색
+	User selectMember(String userId, String userPasswd);
+}
