e50f9efea0ca15f9b9c6a9efdfcce64ef4a3d227
gyumingomin <ool9898@naver.com>
2024-01-02 14:44:35 +0900

    초기설정

--- health_kiosk/src/application/controller/HomeController.java
+++ health_kiosk/src/application/controller/HomeController.java
@@ -3,13 +3,26 @@ package application.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 
-public class HomeController implements Initializable{
+public class HomeController implements Initializable {
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        
-    }
-    
+	@FXML
+	private ComboBox<String> comboBox;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		ObservableList<String> oldList = comboBox.getItems();
+		System.out.println(oldList);
+		ObservableList<String> newList = FXCollections.observableArrayList("고객 관리", "출석부");
+		comboBox.setItems(newList);
+
+	}
+
 }
+
