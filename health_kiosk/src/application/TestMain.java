a307c6658394ec996bd8f2a638106ba4950129af
gyumingomin <ool9898@naver.com>
2024-01-04 09:35:42 +0900

    locker db 설정 완료

--- health_kiosk/src/application/TestMain.java
+++ health_kiosk/src/application/TestMain.java
@@ -10,10 +10,10 @@ public class TestMain extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/fxml/LogonPage.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/fxml/LockerPage.fxml"));
 			Parent root = loader.load();
 			primaryStage.setScene(new Scene(root));
-			primaryStage.setTitle("Logon 페이지");
+			primaryStage.setTitle("페이지");
 			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch (Exception e) {
---
+++ health_kiosk/src/application/dao/LockerDAOImpl.java
@@ -0,0 +1,5 @@
+package application.dao;
+
+public class LockerDAOImpl implements LockerDAO{
+    
+}
