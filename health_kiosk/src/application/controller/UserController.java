e4d800ae87f5d5c8e77ad6851df8729fe30d194e
gyumingomin <ool9898@naver.com>
2024-01-07 22:37:33 +0900

    형님 실수 fxml 파일에 controller 설정 안해놨었네여 수정했어여

--- health_kiosk/src/application/controller/UserController.java
+++ health_kiosk/src/application/controller/UserController.java
@@ -1,6 +1,47 @@
 package application.controller;
 
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import application.dao.UserDAO;
+import application.dao.UserDAOImpl;
+import application.dto.UserChild;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 // 고객관리>회원 페이지
-public class UserController {
+public class UserController implements Initializable {
+
+	@FXML
+    private Label userName;
     
+    @FXML
+    private TextField userNo, userGender, userPhone, userDate;
+    
+    UserDAO dao = new UserDAOImpl();
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		// 계속 해봐도 안됨...뭐가 잘못 되었을까...
+		List<UserChild> userNameList = dao.userManage();
+		if(!userNameList.isEmpty()) {
+			String userName1 = userNameList.get(2).getUserName() +"님";
+			userName.setText(userName1);
+		}
+		
+		
+		
+				
+	}
+	
 }
+		
+		
+    
+	
+	
+
